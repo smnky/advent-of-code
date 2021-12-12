cf51bdc514fa90f77da9fbbe400c4a1f4d8413c2
Koya Sumino <aka@smnky.com>
2021-12-12 11:15:41 +0900

    cljfmt

--- clojure/2021/day01.clj
+++ clojure/2021/day01.clj
@@ -1,23 +1,31 @@
 #!/usr/bin/env bb
 (ns day01
-  (:require [clojure.string :as str]))
+  (:require
+    [clojure.string :as str]))
 
-(def input (->> "input/2021/day01.txt"
-                slurp
-                str/split-lines
-                (mapv parse-long)))
 
-(defn part1 [input]
+(def input
+  (->> "input/2021/day01.txt"
+       slurp
+       str/split-lines
+       (mapv parse-long)))
+
+
+(defn part1
+  [input]
   (->> input
        (partition 2 1)
        (filter #(apply < %))
        count))
 
-(defn part2 [input]
+
+(defn part2
+  [input]
   (->> input
        (partition 3 1)
        (mapv #(apply + %))
        part1))
 
-(prn (str "Part 1: " (part1 input)))
-(prn (str "Part 2: " (part2 input)))
+
+(println (str "Part 1: " (part1 input)))
+(println (str "Part 2: " (part2 input)))
--- clojure/2021/day02.clj
+++ clojure/2021/day02.clj
@@ -1,12 +1,17 @@
 #!/usr/bin/env bb
 (ns day02
-  (:require [clojure.string :as str]))
+  (:require
+    [clojure.string :as str]))
 
-(def input (->> "input/2021/day02.txt"
-                slurp
-                str/split-lines))
 
-(defn parse [line]
+(def input
+  (->> "input/2021/day02.txt"
+       slurp
+       str/split-lines))
+
+
+(defn parse
+  [line]
   (let [[_ direction unit-str] (re-find #"(\w+) (\d+)" line)
         unit (parse-long unit-str)]
     (case direction
@@ -15,7 +20,9 @@
       "forward" {:horizontal unit}
       nil)))
 
-(defn part1 [input]
+
+(defn part1
+  [input]
   (let [sum (->> input
                  (map parse)
                  (apply merge-with +))
@@ -23,4 +30,5 @@
         horizontal (get sum :horizontal 0)]
     (* depth horizontal)))
 
-(prn (str "Part 1: " (part1 input)))
+
+(println (str "Part 1: " (part1 input)))
