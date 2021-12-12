7b4fd3ade4586155ec5372048c66a2d9b64e556e
smnky <aka@smnky.com>
2021-12-12 12:00:42 +0900

    Day2, Step2

--- clojure/2021/day02.clj
+++ clojure/2021/day02.clj
@@ -35,4 +35,27 @@
        multiply))
 
 
+(defn- step-fn
+  [{aim :aim, depth-acc :depth, horizontal-acc :horizontal
+    :or {aim 0, depth-acc 0, horizontal-acc 0}, :as acc}
+   {:keys [depth horizontal]}]
+  (cond
+    depth {:aim (+ aim depth)
+           :depth depth-acc
+           :horizontal horizontal-acc}
+    horizontal {:aim aim
+                :depth (+ depth-acc (* aim horizontal))
+                :horizontal (+ horizontal-acc horizontal)}
+    :else acc))
+
+
+(defn part2
+  [input]
+  (->> input
+       (map parse)
+       (reduce step-fn)
+       multiply))
+
+
 (println (str "Part 1: " (part1 input)))
+(println (str "Part 2: " (part2 input)))
